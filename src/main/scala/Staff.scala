2b62b6c6269a0a48f6253ff930a08216e089bc2a
adamdye12 <adam.dye@qa.com>
2017-06-20 17:31:21 +0000

    staff

--- src/main/scala/Staff.scala
+++ src/main/scala/Staff.scala
@@ -1,6 +1,13 @@
 /**
   * Created by Lewis on 19/06/2017.
   */
-class Staff(id: Int, name: String, jobTitle: String) {
+class Staff(staffId: Int, firstName: String, surname: String, jobTitle: String) {
+  def GetID(): Int = staffId
+
+  def GetFirstName(): String = firstName
+
+  def GetSurname(): String = firstName
+
+  def GetJobTitle(): String = jobTitle
 
 }
