7e22615fbd343caacb519a5000a664f0479eb664
flatuelntwalrus <levelupit@outlook.com>
2017-06-20 10:04:21 +0000

    Test to see if branch works

--- src/main/scala/Customer.scala
+++ src/main/scala/Customer.scala
@@ -2,6 +2,5 @@
   * Created by Lewis on 19/06/2017.
   */
 class Customer(id: Int, name: String, email: String, isLoyalCustomer: Boolean, loyaltyPoints: Int) {
-  var test = "Thanks Obama"
-  println(test)
+
 }
