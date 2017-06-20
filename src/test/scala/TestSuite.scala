92cd1b91c443d8e0028d5495b6ebb9aeda089d08
flatuelntwalrus <levelupit@outlook.com>
2017-06-20 15:35:48 +0000

    Test suite work v1

--- src/test/scala/TestSuite.scala
+++ src/test/scala/TestSuite.scala
@@ -9,10 +9,14 @@ class TestSuite extends FunSuite {
     val theStore = new Store("test", null, null)
     assert(theStore.getID() == "test")
     assert(theStore.getSales() == null)
+    assert(theStore.getStock() == null)
   }
   test("Customer class exists") {
     val theCustomer = new Customer(1, "Elliott", "Xx_WomackRocks_xX@Womack.Me", true, 9001)
-    assert(theCustomer. == "test")
+    assert(theCustomer.GetID() == 1)
+    assert(theCustomer.GetName() == "Elliott")
+    assert(theCustomer.GetEmail() == "Xx_WomackRocks_xX@Womack.Me")
+    assert(theCustomer.GetIsLoyalCustomer() == true)
+    assert(theCustomer.GetLoyaltyPoints() == 9001)
   }
 }
-//id: Int, name: String, email: String, isLoyalCustomer: Boolean, loyaltyPoints: Int
