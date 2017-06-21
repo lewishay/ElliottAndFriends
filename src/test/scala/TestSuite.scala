f7106a3498447beee5b362f7e2972bca9e83df1e
Megalawls <daniel.russell@jrweb.org.uk>
2017-06-21 16:52:26 +0100

    Adding functionality to delete stock, customers, and staff and the tests to check that is works and comes up with a message if a type mismatch

--- src/test/scala/TestSuite.scala
+++ src/test/scala/TestSuite.scala
@@ -11,7 +11,7 @@ import scala.collection.mutable.ArrayBuffer
   */
 class TestSuite extends FunSuite {
   test("Store class exists") {
-    val theStore = new Store("test", null, null, null, null, null)
+    val theStore = Store("test", null, null, null, null, null)
     assert(theStore.id == "test")
     assert(theStore.basket == null)
     assert(theStore.listOfSales == null)
@@ -20,34 +20,61 @@ class TestSuite extends FunSuite {
   }
 
   test("Customer class exists") {
-    val theCustomer = new Customer(1, "Elliott", "Xx_WomackRocks_xX@Womack.Me", true, 9001)
+    val theCustomer = Customer(1, "Elliott", "Xx_WomackRocks_xX@Womack.Me", true, 9001)
     assert(theCustomer.id == 1)
     assert(theCustomer.name == "Elliott")
     assert(theCustomer.email == "Xx_WomackRocks_xX@Womack.Me")
-    assert(theCustomer.isLoyalCustomer == true)
+    assert(theCustomer.isLoyalCustomer)
     assert(theCustomer.loyaltyPoints == 9001)
   }
 
   test("Staff class exists") {
-    val theStaff = new Staff(1, "Iain", "Fraser", "Staff")
+    val theStaff = Staff(1, "Iain", "Fraser", "Staff")
     assert(theStaff.staffId == 1)
     assert(theStaff.firstName == "Iain")
     assert(theStaff.surname == "Fraser")
     assert(theStaff.jobTitle == "Staff")
   }
 
+  test("Cased delete function works"){
+    val store = Store("Elliott & Friends", ArrayBuffer.empty , ArrayBuffer.empty, ArrayBuffer.empty , ArrayBuffer.empty, ArrayBuffer.empty)
+
+    store.staff += Staff(1, "Jane", "Doe", "Manager")
+    assert (store.staff.nonEmpty)
+    store.heldStock += Stock(1, 20.00, 10.00, 5, "Game", "Nier: Automata", " ", LocalDate.now)
+    assert (store.heldStock.nonEmpty)
+    store.customers += Customer(1, "Jane Doe", "jane.doe@gmail.com", true, 100)
+    assert (store.customers.nonEmpty)
+
+
+    val jane: Staff = Staff(1, "Jane", "Doe", "Manager")
+    store.delete(jane)
+    assert (store.staff.isEmpty)
+
+    val nier: Stock = Stock(1, 20.00, 10.00, 5, "Game", "Nier: Automata", " ", LocalDate.now)
+    store.delete(nier)
+    assert (store.heldStock.isEmpty)
+
+    val janedoe: Customer = Customer(1, "Jane Doe", "jane.doe@gmail.com", true, 100)
+    store.delete(janedoe)
+    assert (store.customers.isEmpty)
+
+    store.delete("{ { { { { C U R L Y B O I S } } } } }")
+  }
+
+
   test("Sale is made") {
     val dateTime = LocalDateTime.of(2017, 5, 16, 8, 30)
-    val sale = new Sale(1, dateTime, new ArrayBuffer[Stock], null, 50.0)
-    val store = new Store("1", ArrayBuffer.empty , ArrayBuffer.empty, ArrayBuffer.empty , ArrayBuffer.empty, ArrayBuffer.empty)
+    val sale = Sale(1, dateTime, new ArrayBuffer[Stock], null, 50.0)
+    val store = Store("1", ArrayBuffer.empty , ArrayBuffer.empty, ArrayBuffer.empty , ArrayBuffer.empty, ArrayBuffer.empty)
     store.listOfSales += sale
     assert(store.listOfSales.length == 1)
   }
 
   test("Basket is clear") {
     val date = LocalDate.of(2017, 4, 17)
-    val store = new Store("1", ArrayBuffer.empty , ArrayBuffer.empty, ArrayBuffer.empty , ArrayBuffer.empty, ArrayBuffer.empty)
-    val x = new Stock(1, 35.0, 30.0, 100, "Game", "Nioh", "Playstation 4 age 18", date)
+    val store = Store("1", ArrayBuffer.empty , ArrayBuffer.empty, ArrayBuffer.empty , ArrayBuffer.empty, ArrayBuffer.empty)
+    val x = Stock(1, 35.0, 30.0, 100, "Game", "Nioh", "Playstation 4 age 18", date)
     store.basket += x
     assert(store.basket.length == 1)
     store.clearBasket()
@@ -55,7 +82,7 @@ class TestSuite extends FunSuite {
   }
 
   test("Calculate today's profits"){
-    var store = new Store("Elliott & Friends", ArrayBuffer.empty , ArrayBuffer.empty, ArrayBuffer.empty , ArrayBuffer.empty, ArrayBuffer.empty)
+    var store = Store("Elliott & Friends", ArrayBuffer.empty , ArrayBuffer.empty, ArrayBuffer.empty , ArrayBuffer.empty, ArrayBuffer.empty)
     var todaysSales: ArrayBuffer[Sale] = ArrayBuffer.empty
     todaysSales += Sale(
       1,
@@ -75,7 +102,7 @@ class TestSuite extends FunSuite {
   }
 
   test("List today's sales"){
-    var store = new Store("Elliott & Friends", ArrayBuffer.empty , ArrayBuffer.empty, ArrayBuffer.empty , ArrayBuffer.empty, ArrayBuffer.empty)
+    var store = Store("Elliott & Friends", ArrayBuffer.empty , ArrayBuffer.empty, ArrayBuffer.empty , ArrayBuffer.empty, ArrayBuffer.empty)
     var todaysSales: ArrayBuffer[Sale] = ArrayBuffer.empty
     todaysSales += Sale(
       1,
