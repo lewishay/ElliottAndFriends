d28ee6501ad55e47c29cf710e51ff16d20567ac9
Iainf123 <iainf1@icloud.com>
2017-06-21 12:53:29 +0000

    commited 17:30

--- src/main/scala/Sale.scala
+++ src/main/scala/Sale.scala
@@ -2,15 +2,34 @@ import java.time.LocalDateTime
 import scala.collection.mutable.ArrayBuffer
 
 /**
-  * Created by Lewis on 19/06/2017.
+  * Created by Iain on 21/06/2017.
   */
 class Sale(id: Int, timeOfSale: LocalDateTime, listOfItems: ArrayBuffer[Stock],
-           var totalPrice: Double, customer: Customer = null) {
-  for(item <- listOfItems) totalPrice = totalPrice + item.salePrice
+           var totalPrice: Double = 0, customer: Customer = null) {
+  for (item <- listOfItems) totalPrice = totalPrice + item.salePrice
+  if (customer != null) {
+    customer.loyaltyPoints = totalPrice.toInt + 10
+  }
 
 
-  def generateReceipt(timeOfSale: LocalDateTime, listOfItems: ArrayBuffer[Stock], totalPrice: Double)
+  def generateReceipt(): Unit = {
 
+    println("the time of this sale is" + timeOfSale)
+    println("the item you are buying" + listOfItems)
+    println("the total cost of this product is" + totalPrice)
+    println("this item could of gotten you" + (totalPrice + 10))
 
-  def canPayWithLoyalty(customer: Customer): Boolean
+  }
+}
+
+def canPayWithLoyalty(customer: Customer,totalPrice: Double, isLoyalCustomer : Boolean): Unit = {
+  if (customer != null && isLoyalCustomer == true && customer.loyaltyPoints >= totalPrice) {
+
+    customer.loyaltyPoints - item.saleprice = totalPrice
+
+    println("your loyalty points have been used to pay for your item" + customer.loyaltyPoints)
+  }
+  else {
+    println("you do not have enough loyalty points, show me the scrilla")
+  }
 }
